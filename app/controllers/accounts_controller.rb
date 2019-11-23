8ada2c7a1f1e57877708372e4a1064a2463299db
archetype2142 <malhotraritwick2011@gmail.com>
2019-11-23 21:23:54 +0100

    ASD

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -1,5 +1,6 @@
 class AccountsController < ApplicationController
 	def index
+		puts "YAHAN ACCOUNTS CONTROLLER #{params}"
 		res = FetchBanksJob.new.perform(params[:id])
 
 		puts res
--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,6 +2,7 @@ class CallbackController < ApplicationController
 	def index
 		token = FetchTokenJob.new.perform(params[:code])
 
+		puts "YAHAN CALLBACK CONTROLLER #{token}"
 		redirect_to accounts_path(token: token)
 	end
 end
