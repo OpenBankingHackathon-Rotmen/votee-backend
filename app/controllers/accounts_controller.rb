d0fe566c8c1071f40ced321c5d132c772357f08d
archetype2142 <malhotraritwick2011@gmail.com>
2019-11-23 21:57:26 +0100

    ASD

--- app/controllers/accounts_controller.rb
+++ app/controllers/accounts_controller.rb
@@ -2,7 +2,7 @@ class AccountsController < ApplicationController
 	def index
 		res = FetchBanksJob.new.perform(params[:token])
 
-		redirect_to account_path(id: res, params[:token])
+		redirect_to account_path(id: res, token: params[:token])
 	end
 
 	def show 
