64227ffbaf7e672d3702f9f2b803a0567e326d80
archetype2142 <malhotraritwick2011@gmail.com>
2019-11-23 21:18:58 +0100

    SAD

--- app/controllers/callback_controller.rb
+++ app/controllers/callback_controller.rb
@@ -2,6 +2,6 @@ class CallbackController < ApplicationController
 	def index
 		token = FetchTokenJob.new.perform(params[:code])
 
-		redirect_to account_path(token)
+		redirect_to account_path(id: token)
 	end
 end
